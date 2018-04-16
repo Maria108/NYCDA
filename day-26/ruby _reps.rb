011853313c66f85f97706cbd826e86ccccb4f01b
Maria108 <maria.abash@gmail.com>
2018-04-15 23:48:42 -0400

    Day 26

--- day-26/ruby _reps.rb
+++ day-26/ruby _reps.rb
@@ -1,10 +1,10 @@
-# 1 - Sales Tax 
-# Make a ruby function that adds 8.8% tax if given a number. If it’s given a string it should puts a message asking for a number instead and return nil. 
+# 1 - Sales Tax
+# Make a ruby function that adds 8.8% tax if given a number. If it’s given a string it should puts a message asking for a number instead and return nil.
 
-def sales_tax(arg) 
+def sales_tax(arg)
     if arg.class == Fixnum
          puts arg * 0.88
-    else 
+    else
         puts "Give me a number"
     end
 end
@@ -17,7 +17,7 @@ puts sales_tax(5)
 
 def instance_counter (str)
     # split to aray each char without space
-    array = str.split(" ").join("").split("") 
+    array = str.split(" ").join("").split("")
     counter = 1
     # find only reapeted char more then 1 times
     newArr = array.select { |e| array.count(e) > counter}.uniq
@@ -35,16 +35,15 @@ end
 puts instance_counter("Bob is a very nice gardeneree")
 
 # 3 - Snake Eyes
-# Make a ruby function that rolls two dice 100 times and puts how many times it rolled snake eyes (two 1s). The rand function would be super helpful for this (https://ruby-doc.org/core-2.2.0/Random.html#method-i-rand ). 
+# Make a ruby function that rolls two dice 100 times and puts how many times it rolled snake eyes (two 1s). The rand function would be super helpful for this (https://ruby-doc.org/core-2.2.0/Random.html#method-i-rand ).
 
 def snake_eyes
-    dice1 = Random.new
-    dice2 = Random.new
+    random = Random.new
     counter = 0
     100.times do
-        first_dice = dice1.rand(1...6)
-        second_dice = dice2.rand(1...6)
-        if (first_dice === 1 && second_dice === 1)
+        first_dice = random.rand(1...6)
+        second_dice = random.rand(1...6)
+        if (first_dice == 1 && second_dice == 1)
             counter += 1
         end
     end
